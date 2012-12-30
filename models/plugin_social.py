d2d78017ff05c26e365874816f56c0c26adc913b
rochacbruno <rochacbruno@vaiobang>
2012-12-30 04:42:56 -0200

    source is a SQL all reserved keyword

--- models/plugin_social.py
+++ models/plugin_social.py
@@ -4,8 +4,8 @@ To use this plugin you need a database called db, you need auth
 
 
 db.define_table('plugin_social_link',
-    Field('source','reference auth_user'),
-    Field('target','reference auth_user'),
+    Field('source_user','reference auth_user'),
+    Field('target_user','reference auth_user'),
     Field('accepted','boolean',default=False))
 
 def plugin_social_friend_button(
@@ -39,33 +39,33 @@ def plugin_social_search():
 
 def plugin_social_friends():
     u, l, me = db.auth_user, db.plugin_social_link, auth.user.id
-    friends = db(u.id==l.source)(l.target==me).select(orderby=u.first_name|u.last_name)
-    requests = db(u.id==l.target)(l.source==me).select(orderby=u.first_name|u.last_name)
+    friends = db(u.id==l.source_user)(l.target_user==me).select(orderby=u.first_name|u.last_name)
+    requests = db(u.id==l.target_user)(l.source_user==me).select(orderby=u.first_name|u.last_name)
     return locals()
 
 def plugin_social_callback():
     """AJAX callback!"""
     link, me = db.plugin_social_link, auth.user.id
-    a0,a1 = request.args(0),request.args(1,cast=int)
+    a0,a1 = request.args(0),request.args(1, cast=int)
     if request.env.request_method!='POST': raise HTTP(400)
-    if a0=='request' and not link(source=a1,target=me):
+    if a0 == 'request' and not link(source_user=a1, target_user=me):
         # insert a new friendship request
-        link.insert(source=me,target=a1)
-    elif a0=='accept':
+        link.insert(source_user=me,target_user=a1)
+    elif a0 == 'accept':
         # accept an existing friendship request
-        db(link.target==me)(link.source==a1).update(accepted=True)
-        if not db(link.source==me)(link.target==a1).count():
-            link.insert(source=me,target=a1)
+        db(link.target_user==me)(link.source_user==a1).update(accepted=True)
+        if not db(link.source_user==me)(link.target_user==a1).count():
+            link.insert(source_user=me,target_user=a1)
     elif a0=='deny':
         # deny an existing friendship request
-        db(link.target==me)(link.source==a1).delete()
+        db(link.target_user==me)(link.source_user==a1).delete()
     elif a0=='revoke':
         # delete a previous friendship request
-        db(link.source==me)(link.target==a1).delete()
+        db(link.source_user==me)(link.target_user==a1).delete()
     return 'ok'
 
 def plugin_social_friend_ids(user_id=auth.user_id):
-    return [row.id for row in db(db.plugin_social_link.source==user_id)\
+    return [row.id for row in db(db.plugin_social_link.source_user==user_id)\
                 (db.plugin_social_link.approved==True).select()]
 
 plugin_social_menu = [
